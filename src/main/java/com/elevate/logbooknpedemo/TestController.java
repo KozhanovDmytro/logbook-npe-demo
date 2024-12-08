14092da09c34c4d271c26d52915447fed3568e49
Dmytro Kozhanov <dmytro.kozhanov@intellias.com>
2024-12-08 16:24:09 +0100

    stream endpoint.

--- src/main/java/com/elevate/logbooknpedemo/TestController.java
+++ src/main/java/com/elevate/logbooknpedemo/TestController.java
@@ -1,16 +1,28 @@
 package com.elevate.logbooknpedemo;
 
+import java.time.Duration;
+import java.time.Instant;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/time")
 public class TestController {
 
   @GetMapping
-  public String testEndpoint() {
-    return "test";
+  public String getTime() {
+    return Instant.now().toString();
+  }
+
+  @GetMapping("/stream")
+  public Flux<ServerSentEvent<String>> getTimeStream() {
+    return Flux.interval(Duration.ofSeconds(1L))
+        .map(iteration -> ServerSentEvent.<String>builder()
+            .data(Instant.now().toString())
+            .build());
   }
 
 }
