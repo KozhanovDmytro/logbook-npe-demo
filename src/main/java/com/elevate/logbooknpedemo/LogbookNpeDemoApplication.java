beef2be0b49b522ae86c2afa94963de477d57b72
Dmytro Kozhanov <dmytro.kozhanov@intellias.com>
2024-12-08 15:19:08 +0100

    Initial commit.

---
+++ src/main/java/com/elevate/logbooknpedemo/LogbookNpeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.elevate.logbooknpedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbookNpeDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LogbookNpeDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/elevate/logbooknpedemo/LogbookNpeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevate.logbooknpedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbookNpeDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
